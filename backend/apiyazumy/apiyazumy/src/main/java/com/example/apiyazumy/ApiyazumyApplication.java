603ea464c349962a1d32768df1211067fdf9cd20
Julianquispe122333 <Jhulmervalenzuela2@gmail.com>
2026-06-15 10:05:14 -0500

    Integracion Spring Boot backend inicial

---
+++ backend/apiyazumy/apiyazumy/src/main/java/com/example/apiyazumy/ApiyazumyApplication.java
@@ -0,0 +1,13 @@
+package com.example.apiyazumy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiyazumyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiyazumyApplication.class, args);
+	}
+
+}
